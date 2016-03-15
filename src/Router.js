7a2528cff37fb6c9ba10a724d58aece0096d5eaf
Timur Uzel <Timur Uzel>
2016-03-14 22:22:48 -0400

    test

--- src/Router.js
+++ src/Router.js
@@ -1,3 +1,15 @@
 /**
  * Created by timur on 3/14/2016.
  */
+
+export default class Router {
+
+  constructor() {
+    console.log('Router created');
+  }
+
+  route(path) {
+    console.log(`Route to  ${path}`);
+  }
+
+}
